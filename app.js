252c825967dd5d22a9e7533574aa237d7ed6933f
Connie <constancecrawford87@gmail.com>
2021-10-19 09:45:38 -0400

    looping

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 var ourArray = [];
-for(var i = 0; i <= 10; i++) {
+for(var i = 1; i <= 10; i++) {
     ourArray.push(i);
 }
 var ourArray = [];
-var i = 0;
-while(i <= 10) {
+var i = 10;
+while(i >= 1) {
     ourArray.push(i);
     i--;
 }
 
 var ourArray = [];
-for (var i = 0; i => 21; i += 2) {
+for (var i = 0; i <= 21; i += 2) {
     ourArray.push(i);
 }
 
 var ourArray = [];
-for (var i = 15; i > 0; i -= 2) {
+for (var i = 15; i >= 1; i -= 2) {
     ourArray.push(i);
 }
 
