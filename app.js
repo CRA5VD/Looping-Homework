8b7b62b6ef11e0b5baa29091579bcb3da9aceb42
Connie <constancecrawford87@gmail.com>
2021-10-19 09:36:19 -0400

    pushing loops

---
+++ app.js
@@ -0,0 +1,23 @@
+var ourArray = [];
+for(var i = 0; i <= 10; i++) {
+    ourArray.push(i);
+}
+var ourArray = [];
+var i = 0;
+while(i <= 10) {
+    ourArray.push(i);
+    i--;
+}
+
+var ourArray = [];
+for (var i = 0; i => 21; i += 2) {
+    ourArray.push(i);
+}
+
+var ourArray = [];
+for (var i = 15; i > 0; i -= 2) {
+    ourArray.push(i);
+}
+
+
+
